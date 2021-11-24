4c755d6e99e60c232cdb95a5e54ad223452dc2ec
Leam Hall <leamhall@gmail.com>
2021-11-24 16:42:05 -0600

    data cleanup

--- resume_writer.py
+++ resume_writer.py
@@ -3,23 +3,24 @@
 import re
 import os
 
-datadir = "data"
-ce_text_string = "{} {} {}\n"
-ce_html_string = "<p><b>{}</b> {} {}</p>\n"
+datadir         = "data"
+outputdir       = "output"
+ce_text_string  = "{} {} {}\n"
+ce_html_string  = "<p><b>{}</b> {} {}</p>\n"
 ce_file = "continuing_education.txt"
 ### Sample lines (no '#'):
 # Python:
 #  Learn to Program, The Fundamentals (Python) (Coursera) (January 2014)
 
-ce_text_out = 'ce.txt'
-ce_html_out = 'ce.html'
-ce_text_fh = open(ce_text_out, 'w')
-ce_html_fh = open(ce_html_out, 'w')
+ce_text_out = os.path.join(outputdir, 'ce.txt')
+ce_html_out = os.path.join(outputdir, 'ce.html')
+ce_text_fh  = open(ce_text_out, 'w')
+ce_html_fh  = open(ce_html_out, 'w')
 
 class ContEd():
   def __init__(self, dataline):
-    parts = dataline.split(" ")
-    self.year = parts[-1].replace(')','')
+    parts       = dataline.split(" ")
+    self.year   = parts[-1].replace(')','')
     self.source = parts[-3]
     self.course = " ".join(parts[:-3])
 
@@ -28,9 +29,9 @@ def cont_ed_out(ce, format_string, filehandle):
   filehandle.write(format_string.format(ce.year, ce.course, ce.source))
 
 def list_from_file(directory, file):
-  filename	= os.path.join(datadir, file)
-  file = open(filename, "r")
-  lines = file.readlines()
+  filename  = os.path.join(datadir, file)
+  file      = open(filename, "r")
+  lines     = file.readlines()
   file.close()
 
   data = [] 
@@ -41,7 +42,7 @@ def list_from_file(directory, file):
   return data
 
 def sorted_list(data, sort_attrib, direction = False):
-  stuff = {}
+  stuff       = {}
   return_list = []
   for d in data:
     # need a way to translate the sort_attrib to "year".
@@ -62,7 +63,6 @@ for l in ce_data_file:
   if re.search('[0-9]{4}', l):
     ce = ContEd(l)	
     ce_data.append(ce) 
-
 
 rev_sorted_ce = sorted_list(ce_data, 'year')
 for ce in rev_sorted_ce:
