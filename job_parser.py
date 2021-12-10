13f0d8344a6d1fd99626c145155f00148fad31c2
Leam Hall <leamhall@gmail.com>
2021-12-10 10:47:18 -0600

    Added job_parser.py.

---
+++ job_parser.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+
+# name:     job_parser.py
+# version:  0.0.1
+# date:     20211210
+# author:   Leam Hall
+# desc:     Parse job section of resume to individual files.
+
+### Notes:
+#
+#  1. Assumes we have a text file of just the job data. Example (no # marks):
+
+#     DevOps Engineer (cloud), Big Corp (SubContractor) (Aug 2010 - Feb 2013) 
+#
+#     Did great stuff and looked really cool the entire time. Blah, blah, blah.
+
+#  2. The header format is what I use, but needs to be configurable.
+
+import json
+import os
+import re
+
+# These need to be command line options.
+job_filename    = 'data/jobs.txt'
+jobs_dir        = 'data/jobs'
+jobs_json_file  = os.path.join(jobs_dir, "jobs.json")
+jobs_dict       = {}
+
+# This needs to be configurable.
+header_line   = '#title:customer:start_date:end_date'
+
+
+def write_file(data):
+  ''' string, dict => writes to file
+  ''' 
+  try:
+    file_path = os.path.join(jobs_dir, data['filename'])
+    with open(file_path, 'w') as f:
+      f.write("{}\n".format(header_line))
+      header_string = ":".join([data['title'], data['customer'],
+        data['start'], data['stop']])
+      f.write("{}\n".format(header_string))
+      f.write("\n{}\n".format(data['blurb']))
+      f.close()
+  except Exception as e:
+    print(e)
+
+
+def ensure_write_dir(directory):
+  ''' directory name => creates if not exist, verifies dir and writeable.
+    else raise exception.
+  '''
+  if not os.path.isdir(directory):
+    try:
+      os.mkdir(directory, 0o0755)
+    except OSError as ose:
+      print("Cannot create directory")
+      os.exit(1)
+
+
+def make_key_filename(data):
+  ''' dict => strings for dict key, filename '''
+  customer  = data['customer']
+  customer  = re.sub("\(", '', customer)
+  customer  = re.sub("\)", '', customer)
+  customer  = re.sub(" ", "_", customer)
+  customer  = re.sub("\.", "", customer)
+  customer  = customer.lower()
+  key       = "{}_{}_{}".format(data['start'], data['stop'], customer)
+  filename  = "{}.txt".format(key)
+  return key, filename
+
+
+def parse_title_line(line):
+  data              = {}
+  return_line       = ''
+  line_split        = line.split(",")
+  data['title']     = line_split[0].strip()
+  job_and_dates     = line_split[1].split("(")
+  for index, item in enumerate(job_and_dates):
+    job_and_dates[index] = re.sub("\)", '', item)
+  customer          = job_and_dates[0].strip()
+  if len(job_and_dates) > 2:
+    customer = "{} ({})".format(customer, job_and_dates[1].strip())
+  data['customer']  = customer
+  dates             = job_and_dates[-1].strip()
+  (start, stop)     = re.findall("[\d]{4}", dates)
+  data['start']     = start
+  data['stop']      = stop 
+  return data
+
+ 
+#####
+
+ensure_write_dir(jobs_dir)
+
+try:
+  job_data  = open(job_filename, 'r')
+  j_data    = {}
+  for line in job_data.readlines():
+    line = line.strip()
+    if len(line) < 5: 
+      next
+    elif re.search("[\d]{4}\s*\)", line):
+      j_data                    = parse_title_line(line)
+      j_data['key'], j_data['filename']   = make_key_filename(j_data)
+    else:
+      j_data['blurb']           = line
+      write_file(j_data)
+      jobs_dict[j_data['key']]  = j_data.copy()
+
+  with open(jobs_json_file, 'w') as j:
+    j.write(json.dumps(jobs_dict, indent=4))
+    j.close()
+ 
+except FileNotFoundError as e:
+  print("Can't find file.")
+  os.exit(1)
+
+
